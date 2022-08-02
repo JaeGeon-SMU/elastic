dc3d348001765dcc590dbf0da4825aa30c473861
LeeJaeGeon <dlworjs1021@gmail.com>
2022-08-02 13:18:27 +0900

    make it better3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 
 app.get("/", (req, res) => {
-    res.send('<iframe src="https://hellocustomer.kb.ap-northeast-2.aws.elastic-cloud.com:9243/app/dashboards?auth_provider_hint=anonymous1#/view/0112f870-a37e-11ec-87af-dd68cc767fa7?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))" height="600" width="800"></iframe>');
+    res.send('<iframe src="https://hellocustomer.kb.ap-northeast-2.aws.elastic-cloud.com:9243/app/dashboards?auth_provider_hint=anonymous1#/view/0112f870-a37e-11ec-87af-dd68cc767fa7?embed=true&_g=(filters%3A!()%2CrefreshInterval%3A(pause%3A!t%2Cvalue%3A0)%2Ctime%3A(from%3Anow-15m%2Cto%3Anow))?auth_provider_hint=anonymous1" height="600" width="800"></iframe>');
 });
 
 //middleware
